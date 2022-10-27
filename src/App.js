d0187c0bf6e503199f83884fe1929733c3d45db9
paul yashouh <87258234+payamaya@users.noreply.github.com>
2022-10-27 10:46:10 +0200

    "new paragraph" (#43)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     <div className='App'>
       <h1>Hello world </h1>
       <h6>Heloooooooooooooo</h6>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero vero temporibus molestiae fugiat neque tenetur eum molestias! Possimus, pariatur tempore.</p>
+
       <Login />
     </div>
   )
