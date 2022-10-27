3a338b6c6ef0785c82f64125faa947e5c4c9edb2
Vasiliki83 <vasiliki.panagiotidis@student.kyh.se>
2022-10-27 11:27:44 +0200

    "ändirng i css"

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -16,13 +16,15 @@ const startPage =() => {
                             To complete the game you have to defeat the evil Gururu<br></br>
                             who guards the forest. You will have to make smart choices<br></br>
                             in order to complete the game.<br></br>
+                            The roads will be challenging and somewhat ingenious to pass.<br></br>
+                            <br></br>
                             Hurry, and remember, you only got 3 lives.
                         </p>
             </div>
             
             <div className="startButton">
                         <Link className="links" to = "/login">
-                                <button className="buttonCart">Start game </button>
+                                <button className="buttonStart">Start game </button>
                         </Link>
             </div>
         </div>
