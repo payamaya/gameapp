f46a5e2ce3ccf6bdb879b00d88fb481fce38d379
payamaya <paulyashou81@gmail.com>
2022-10-27 14:30:45 +0200

    feature branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-
- import {BrowserRouter, Routes, Route} from "react-router-dom"
- import React from "react";
- import Login from "./components/Login";
- import StartPage from "./components/StartPage";
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import Login from './components/Login'
+import StartPage from './components/StartPage'
 
 function App() {
   return (
-
     <div>
-    <BrowserRouter>
-
-    <Routes>
-
-      <Route path = "/" element = {<StartPage/>} />
-      <Route path = "/login" element = {<Login/>} />
-
-    </Routes>
-    </BrowserRouter>
-
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<StartPage />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
