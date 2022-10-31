e112b24c6e690e4b2c4f3cd4bfc31236296897a2
JohannaU1 <94961913+JohannaU1@users.noreply.github.com>
2022-10-31 11:27:25 +0100

    Added FlamingLake

--- src/App.js
+++ src/App.js
@@ -6,29 +6,6 @@ import SignUp from './components/SignUp'
 import PlayGame from './components/PlayGame'
 import NotFound from './components/NotFound'
 
-<<<<<<< HEAD
- import {BrowserRouter, Routes, Route} from "react-router-dom"
- import React from "react";
- import Login from "./components/Login";
- import StartPage from "./components/StartPage"
- import MemicPage from "./components/MemicPage";
- import FlamingLakePage from "./components/FlamingLakePage";
-function App() {
-  return (
-    <div>
-    <BrowserRouter>
-
-    <Routes>
-
-      <Route path = "/" element = {<StartPage/>} />
-      <Route path = "/login" element = {<Login/>} />
-      <Route path = "/MemicPage" element = {<MemicPage/>} />
-      <Route path = "/FlamingLakePage" element = {<FlamingLakePage/>} />
-
-    </Routes>
-    </BrowserRouter>
-
-=======
 function App() {
   return (
     <div>
@@ -41,7 +18,6 @@ function App() {
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
->>>>>>> feature-branch
     </div>
   )
 }
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,40 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-<<<<<<< HEAD
-const startPage =() => {
-    return (
-
-        <div className="container">
-
-             <div className="startContainer">
-            
-                    <h1> WELCOME TO THE ADVENTURE </h1>
-            
-
-                    <div className="textStart">
-                         <p> Welcome to the adventure of Memic forest. <br></br>
-                            To complete the game you have to defeat the evil Gururu<br></br>
-                            who guards the forest. You will have to make smart choices<br></br>
-                            in order to complete the game.<br></br>
-                            The roads will be challenging and somewhat ingenious to pass.<br></br>
-                            <br></br>
-                            Hurry, and remember, you only got 3 lives.
-                        </p>
-            </div>
-            
-            <div className="startButton">
-                        <Link className="links" to = "/login">
-                                <button className="buttonStart">Login / Sign up</button>
-                        </Link>
-            </div>
-
-            <div>
-                <Link className="links" to = "/MemicPage">
-                    <button>Memic</button>
-                </Link>
-            </div>
-=======
 const startPage = () => {
   return (
     <div className='container'>
@@ -51,7 +17,6 @@ const startPage = () => {
             <br></br>
             Hurry, and remember, you only got 3 lives.
           </p>
->>>>>>> feature-branch
         </div>
 
         <div className='startButton'>
