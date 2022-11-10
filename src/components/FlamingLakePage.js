69a74d5ebf91638d777cdd8d2876bd7441a330e6
payamaya <paulyashou81@gmail.com>
2022-11-10 10:10:47 +0100

    merging button

--- src/components/FlamingLakePage.js
+++ src/components/FlamingLakePage.js
@@ -1,71 +1,53 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import '../styles/FlamingLake.css'
-const FlamingLakePage =() =>{
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/flaminglake.css'
+const FlamingLakePage = () => {
+  const [left, setLeft] = useState(false)
+  const [right, setRight] = useState(false)
 
-    const [left, setLeft] = useState(false)
-    const [right, setRight] = useState (false)
+  const [leftThree, setLeftThree] = useState(false)
+  const [rightThree, setRightThree] = useState(false)
+  // Set Disable on the next two choice if u are right you disable what the road u passed.
 
-    const [leftThree, setLeftThree] = useState(false)
-    const [rightThree, setRightThree] = useState(false)
-    // Set Disable on the next two choice if u are right you disable what the road u passed.
+  const handleLeft = () => {
+    setLeft(true)
+    alert('Rätt')
+  }
 
-    const handleLeft = () => {
-        setLeft(true);
-        alert("Rätt")
+  const handleRight = () => {
+    setRight(true)
+    alert(' Du förlorade 1 liv ')
+  }
 
-       
-    }
+  const handleLeftLast = () => {
+    setLeft(true)
+    alert('Fel')
+  }
 
-    const handleRight = () =>{
-        setRight(true);
-        alert(" Du förlorade 1 liv ")
-    }
+  const handleRightLast = () => {
+    setRight(true)
+    alert(' du kom vidare Grattis ')
+  }
 
-    const handleLeftLast = () => {
-        setLeft(true);
-        alert("Fel")
+  return (
+    <div className='containerlake'>
+      <div className='FlamingLake-item'>
+        <h2 className='h2'>FlamingLake</h2>
+        <button onClick={handleLeft}>Left</button>
+        <button onClick={handleRight}>Right</button>
+        <br />
 
-       
-    }
+        <button onClick={handleLeft}>Left</button>
+        <button onClick={handleRight}>Right</button>
+        <br />
 
-    const handleRightLast = () =>{
-        setRight(true);
-        alert(" du kom vidare Grattis ")
-    }
-
-    return <div className="containerlake">
-    <div className="FlamingLake-item">
-    <h2 className="h2">FlamingLake</h2>
-    <button onClick={handleLeft}>Left</button>
-    <button onClick={handleRight}>Right</button>
-    <br/>
-
-
-    <button onClick={handleLeft}>Left</button>
-    <button onClick={handleRight}>Right</button>
-    <br/>
-
-
-    <button onClick={handleLeftLast}>Left</button>
-    <Link className="links" to = "/GaruruNest">
-    <button onClick={handleRightLast}>Right</button>
-    </Link>
+        <button onClick={handleLeftLast}>Left</button>
+        <Link className='links' to='/GaruruNest'>
+          <button onClick={handleRightLast}>Right</button>
+        </Link>
+      </div>
     </div>
-
-
-</div>
-
-
-    
-    
+  )
 }
 
-
-
-
-
-
-
-
-export default FlamingLakePage;
+export default FlamingLakePage
